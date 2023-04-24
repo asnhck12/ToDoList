fd554c0a403b5584fce45c71305d9f5b39109490
AH LT <ahussain93@live.co.uk>
2023-04-24 18:01:06 +0100

    Basic infrasctrucure and logo added

---
+++ src/index.js
@@ -0,0 +1,9 @@
+console.log("test");
+
+
+//module for a form that asks for project details: Project Title, Project description, Date Due
+
+
+//module to display the projects, shows Project Title, Project description, Date Due
+
+//module to show the projects and the associated tasks in the main section
