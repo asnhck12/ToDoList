245c2586fc8aaa4d2e5ff8548dcf79625b48d3c5
AH LT <ahussain93@live.co.uk>
2023-12-14 19:35:39 +0000

    Adding to do items majority working, loops for adding items in addtodolist needs fixing

--- src/add-to-do-list.js
+++ src/add-to-do-list.js
@@ -7,14 +7,11 @@ export function addToDo(i, toDoListText, toDoListSubmit, divProjectView, formSub
         const toDoClassArray = [];
         var counter = -1;
         var currentForm = JSON.parse(localStorage.getItem("submissions" + i));
+        var currentFormItem = "submissions" + i;
+
     //Submits a to do item
     toDoListSubmit.addEventListener("click", function() {
         
-        let toDoList = currentForm.toDo;
-        counter ++;
-        console.log(formSubmissionSpecific);
-        console.log(i);
-
         toDoText = toDoListText.value;
         const toDoListDiv = document.createElement('ul');
         toDoListDiv.setAttribute('id', 'toDoList');
@@ -30,19 +27,7 @@ export function addToDo(i, toDoListText, toDoListSubmit, divProjectView, formSub
             localStorage.setItem("submissions" + i, JSON.stringify(currentForm));
             toDoListDiv.appendChild(toDoItem);
             divProjectView.appendChild(toDoListDiv); 
-            // toDoItem.addEventListener("click", function() {
-                editToDoList(currentForm, formSubmissionSpecific);
-                // if (toDoItem.classList.contains("strkthru")){
-                //     toDoItem.classList.remove("strkthru");
-                //     toDoClassArray[counter] = "No";
-                //     currentForm.toDoClass = toDoClassArray;
-                //     localStorage.setItem(("submissions" + i), JSON.stringify(currentForm));    }
-                //     else {
-                //         toDoItem.classList.add("strkthru");
-                //         toDoClassArray[counter] = "Yes";
-                //         currentForm.toDoClass = toDoClassArray;
-                //         localStorage.setItem(("submissions" + i), JSON.stringify(currentForm);
-            // })
+            editToDoList(currentForm, "submissions" + i);
         }
     
         //Adds a new item to an exisiting to-do list stored in localstorage
@@ -57,45 +42,6 @@ export function addToDo(i, toDoListText, toDoListSubmit, divProjectView, formSub
             toDoItem.textContent = toDoText;
             localStorage.setItem("submissions" + i, JSON.stringify(currentForm));
             existingToDoListDiv.appendChild(toDoItem);
-            // toDoItem.addEventListener("click", function() {
-                editToDoList(currentForm, formSubmissionSpecific);
-                // if (toDoItem.classList.contains("strkthru")){
-                //     toDoItem.classList.remove("strkthru");
-                //     toDoClassArray[counter] = "No";
-                //     currentForm.toDoClass = toDoClassArray;
-                //     localStorage.setItem((currentForm), JSON.stringify(currentForm));    }
-                //     else {
-                //         toDoItem.classList.add("strkthru");
-                //         toDoClassArray[counter] = "Yes";
-                //         currentForm.toDoClass = toDoClassArray;
-                //         localStorage.setItem((currentForm), JSON.stringify(currentForm)
-        // });
+            editToDoList(currentForm, "submissions" + i);
                                     }})
             }
-        // })}
-
-        // let toDoLists = toDoListSection.getElementsByTagName('li');
-        // for (let j = 0; j < toDoLists.length; j ++) {
-        //     toDoLists.addEventListener("click", function() {
-        //         if (toDoLists.classList.contains("strkthru")){
-        //             toDoLists.classList.remove("strkthru");
-        //             toDoClassArray[j] = "No";
-        //             currentForm.toDoClass = toDoClassArray;
-        //             localStorage.setItem((currentForm), JSON.stringify(currentForm));
-        //             }
-        //             else {
-        //                 toDoLists.classList.add("strkthru");
-        //                 toDoClassArray[j] = "Yes";
-        //                 currentForm.toDoClass = toDoClassArray;
-        //                 localStorage.setItem((currentForm), JSON.stringify(currentForm));
-        //             }
-                
-                
-            
-            // })}      
-        
-        // let toDoItem = toDoListDiv.getElementsByTagName('li');
-        // for (let i = 0; i < toDoItem.length; i ++) {
-        //     console.log(i);
-        //     editToDoList(counter, currentForm, formSubmissionSpecific, toDoList);
-        //  };
--- src/edit-to-do-list.js
+++ src/edit-to-do-list.js
@@ -1,29 +1,27 @@
 import { toDoListListed } from "./to-do-list";
 
-export function editToDoList (currentForm, formSubmissionSpecific/*, toDoClassArrayCurrent*/) {
+export function editToDoList (currentForm, formSubmissionItem) {
     let toDoListSection = document.getElementById("toDoList");
     let toDoClassArrayCurrent = currentForm.toDoClass;
     
     let toDoListItems = toDoListSection.getElementsByTagName('li');
-    console.log("test");
 
         //Adds or removes the class that is striked through whichever todo is selected
         for (let i = 0; i < toDoListItems.length; i ++) {
-            console.log(toDoListItems[i]);
             toDoListItems[i].addEventListener("click", function() {
                 if (toDoListItems[i].classList.contains("strkthru")){
-                    console.log(toDoListItems[i]);
+                    console.log("contains class " + toDoListItems[i]);
                     toDoListItems[i].classList.remove("strkthru");
                     toDoClassArrayCurrent[i] = "No";
                     currentForm.toDoClass = toDoClassArrayCurrent;
-                    localStorage.setItem((formSubmissionSpecific), JSON.stringify(currentForm));    
+                    localStorage.setItem((formSubmissionItem), JSON.stringify(currentForm));    
                 }
                 else {
-                    console.log(toDoListItems[i]);
+                    console.log("doesnt contain class" + toDoListItems[i]);
                     toDoListItems[i].classList.add("strkthru");
                     toDoClassArrayCurrent[i] = "Yes";
                     currentForm.toDoClass = toDoClassArrayCurrent;
-                    localStorage.setItem((formSubmissionSpecific), JSON.stringify(currentForm));    
+                    localStorage.setItem((formSubmissionItem), JSON.stringify(currentForm));    
     }
    }   )
     }
